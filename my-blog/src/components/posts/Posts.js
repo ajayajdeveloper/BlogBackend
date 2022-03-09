b87b9696917150cd680a2f518e9f4555c3d689fb
Ajaydev <ajaykumaraj2529@gmail.com>
2022-03-09 23:37:49 +0530

    my changes

--- my-blog/src/components/posts/Posts.js
+++ my-blog/src/components/posts/Posts.js
@@ -9,7 +9,7 @@ export default function Posts({ posts }) {
         <div  className='posts'>
         { posts.map((p) => (
           < Post  post={p}  />
-        ))}
+         ))}
         </div>
     )
 }
